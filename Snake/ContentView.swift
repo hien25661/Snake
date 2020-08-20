29e0871986ca07c7de8dda3d70ada1a7f1225705
Nguyen Hien <hiennv@hikesiya.co.jp>
2020-08-20 17:03:47 +0900

    Initial Commit

---
+++ Snake/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Snake
+//
+//  Created by HienNV on 8/20/20.
+//  Copyright © 2020 HienNV. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
